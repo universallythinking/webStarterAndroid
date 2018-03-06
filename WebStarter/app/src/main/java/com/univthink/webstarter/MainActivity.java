fb291d3829fb08f505b70e1d9cf2c8a69396b145
benjaminbarnett <benjamin@Universallythinking.com>
2018-03-06 16:33:28 -0500

    Initial

---
+++ WebStarter/app/src/main/java/com/univthink/webstarter/MainActivity.java
@@ -0,0 +1,17 @@
+package com.univthink.webstarter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final WebView wv = (WebView) findViewById(R.id.webView);
+        final String urlStart = "file:///android_asset/webAssets/index.html";
+        wv.loadUrl(urlStart);
+    }
+}
